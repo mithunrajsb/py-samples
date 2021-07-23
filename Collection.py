0f8a1e6a7cce400baa104cb8651c20cc8a2ca231
mithunrajsb <mrsb@live.in>
2021-07-23 08:17:53 +0530

    Add files via upload

---
+++ Collection.py
@@ -0,0 +1,5 @@
+fruits = ["apple","banana","orange"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+# Sample program in Python
+print("Hello World")
+
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+z = "awsome"
+print ("Python is " + z)
+
+print("type of x is " + str(type(x)))
+print("type of y is " + str(type(y)))
